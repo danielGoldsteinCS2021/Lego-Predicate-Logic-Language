5ca9f27188ad99b557cb5944483c0d153bca1e02
Daniel Goldstein <18djg@queensu.ca>
2021-02-02 15:50:46 -0500

    clean up ast files

--- lego/ast/Rel_formula.java
+++ lego/ast/Rel_formula.java
@@ -1,12 +1,12 @@
 package lego.ast;
 
 public class Rel_formula extends Formula{
-    public Rel_op rel_op;
+    public Rel_op relOp;
     public Expr lhs;
     public Expr rhs;
 
     public Rel_formula(Rel_op rel_op, Expr lhs, Expr rhs){
-        this.rel_op = rel_op;
+        this.relOp = rel_op;
         this.lhs = lhs;
         this.rhs = rhs;
     }
