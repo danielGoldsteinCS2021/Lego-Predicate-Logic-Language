be0a68ebd2de15d8476c91f1f60181f0cc6c2bfe
Daniel Goldstein <18djg@queensu.ca>
2021-02-03 20:13:19 -0500

    works bare bones, need to implement error handling and fix issue with order of operations

--- Main.java
+++ Main.java
@@ -1,27 +1,32 @@
-/***
- * Excerpted from "The Definitive ANTLR 4 Reference",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
- * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
- ***/
 import lego.LegoLexer;
 import lego.LegoParser;
+import lego.ast.ASTBuilder;
+import lego.ast.Expr;
+import lego.ast.Formula;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 import lego.EvalVisitor;
-
+// FIX BEDMAS ISSUE, CURRENTLY NOT DOING BEDMAS
 public class Main {
     public static void main(String[] args) throws Exception {
         String formula = args[0];
         LegoLexer lexer = new LegoLexer(CharStreams.fromString(formula));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         LegoParser parser = new LegoParser(tokens);
+        ASTBuilder builder = new ASTBuilder();
 
-        ParseTree tree = parser.program(); // parse
+        LegoParser.StartContext context = parser.start();
+        if (context.formula() != null) {
+            Formula ast = (Formula) builder.visitFormula(context.formula());
+            boolean result = new EvalVisitor().eval(ast);
+            System.out.println("RESULT IS :   "+result);
+        }
+        else{
+            Expr ast = (Expr) builder.visitExpr(context.expr());
+            int result = new EvalVisitor().eval(ast);
+            System.out.println("RESULT IS :   "+result);
+        }
 
-        EvalVisitor eval = new EvalVisitor();
         //eval.visit(tree);
     }
 }
